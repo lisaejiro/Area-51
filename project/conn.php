41e462330a811d0b3864d56564197c83ba422d89
mslee <lisa_ejiro@yahoo.com>
2019-09-19 14:42:28 +0100

    changes

--- project/conn.php
+++ project/conn.php
@@ -4,7 +4,7 @@ session_start();
 define('DB_SERVER', "localhost");
 define('DB_USER', "root");
 define('DB_PASSWORD', "");
-define('DB_ENGINE', "ruby");
+define('DB_ENGINE', "area51");
 
 
 $conn = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_ENGINE);
